703d2a13ec7672af8afeaf8b252499f962d3749b
0xlilith <0xlilith@protonmail.com>
2021-12-03 23:02:18 +0530

    day2 part 2

---
+++ Day2-part2/soln.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+int main(void) {
+	std :: ifstream file("input.txt");
+	int num=0, depth=0, horizontal=0, aim=0;
+	std :: string arr[2000];
+	while (! file.eof()) {
+		file >> arr[num];
+		num++;
+	}
+	file.close();
+	for(int i=0;i<num;i+=2) {
+		if (arr[i].compare("down") == 0)
+			aim += stoi(arr[i+1]);
+		else if (arr[i].compare("up") == 0)
+			aim -= stoi(arr[i+1]);
+		else if (arr[i].compare("forward") == 0) {
+			horizontal += stoi(arr[i+1]);
+			depth += (stoi(arr[i+1]) * aim);
+		}
+	}
+	std :: cout << horizontal * depth ;
+}
